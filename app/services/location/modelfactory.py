6c825b8de48a5113bc66e40bed7558035f837490
JoyBiao <1161271906@qq.com>
2021-08-15 22:47:25 -0400

    apply bridge pattern under app/services/location directory. class csbs.py, jhu.py, nyt.py implements the interface DataSourcesInterface in sourcelocations.py. Add BasicLocationService class to implement the LocationService class. LocationService class in __init__.py get a new constructor method. And apply BasicLocationService in app/data/__init__.py

--- app/services/location/modelfactory.py
+++ app/services/location/modelfactory.py
@@ -2,11 +2,11 @@ from ...models import *
 class ModelFactory():
     def create_model(self, type, data):
         """Factory Method"""
-        localizers = {
+        models = {
             "timeline": Timeline(timeline = data),
             "latest": Latest(confirmed = data.confirmed, deaths = data.deaths, recovered = data.recovered),
             "timelines": Timelines(confirmed = data.confirmed, deaths = data.deaths, recovered = data.recovered),
             "location": Location(data)
         }
  
-        return localizers[type]
+        return models[type]
